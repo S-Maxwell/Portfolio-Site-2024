bc9f1e18f6e12e0a84a07caee3192cab5264365c
S-Maxwell <156356305+S-Maxwell@users.noreply.github.com>
2024-12-08 02:07:09 -0500

    Update main.js
    In this commit, the main.js file was updated.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,35 @@
+// console.log("JS is running")
+//variables
+//functions
 (function () {
- 
+    const player = new Plyr('video');
+    
+    // const letsTalkBtn = document.querySelector("#btn-1");
 
+    // // const contactSection = document.querySelector("#contact");
+    // // const homeLink = document.querySelector('a[href="#home"]');
+    // // const moreAboutMeBtn = document.querySelector('.more-abt');
+
+    // // function scrollToContact() {
+    // //     contactSection.scrollIntoView({
+    // //         behavior: 'smooth'
+    // //     });
+    // // }
+
+    // // function preventDefaultAndScroll(e) {
+    // //     e.preventDefault();
+    // //     const targetId = e.currentTarget.getAttribute('href');
+    // //     smoothScroll(targetId);
+    // // }
+
+    
+
+    // // //event listeners
+    // // letsTalkBtn.addEventListener("click", scrollToContact);
+
+    // //  if (homeLink) homeLink.addEventListener('click', preventDefaultAndScroll);
+     
+    // //  if (moreAboutMeBtn) {
+    // //     moreAboutMeBtn.addEventListener('click', () => smoothScroll('#about'));
+    // // }
 })();
